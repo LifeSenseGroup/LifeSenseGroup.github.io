6ae43a729c270d972fcc11e99408a4b83886eb82
LifeSenseGroup <info@lifesense-group.com>
2020-01-19 20:38:13 +0100

    first part of lsg webapp

---
+++ precache-manifest.d7b1eb2819eb43c1f842eba90a96e5f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "893102e1869adc5bce0a94feb6f74249",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "191c039bf63d96535107",
+    "url": "main.css"
+  },
+  {
+    "revision": "da48dd1cd76dee364cc403d607193f16",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "8391957aaff84ed2243736878b5b1108",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "4d11214a32273e9537b2b7f085807ebd",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "abb358dc9afad389c31bee5903095e06",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "449e154f5a93678178caf6d1d5857d65",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "cea8f68ecc2d0e354b643d277bbd5d8b",
+    "url": "index.html"
+  },
+  {
+    "revision": "265a4a61a2d0cb79fe2f716e12029a29",
+    "url": "img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "22cf434c35485a385ee356882fbbacb3",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "0421b4b74aa24572c428a97bc19880eb",
+    "url": "img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "028be18872917eee9d192f7ef2c7d354",
+    "url": "img/icons/favicon.png"
+  },
+  {
+    "revision": "191c039bf63d96535107",
+    "url": "bundle.js"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,8 @@
+importScripts("precache-manifest.d7b1eb2819eb43c1f842eba90a96e5f6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+if ('workbox' in self) {
+    workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+  }
+
+
+  123234
