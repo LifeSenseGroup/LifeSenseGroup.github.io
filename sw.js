8f87c27974230a18f29100977beee1ed3917bd27
marc <info@lifesense-group.com>
2020-04-16 09:59:06 +0200

    test new webpack plugin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.3cbe1694ccf40e662fffeb380a2693ee.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.ad89e0643637af6c29a5ad7a7a9795a7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
