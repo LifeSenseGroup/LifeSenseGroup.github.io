4c68c9a48872fba2dc9ee3426c947fe5cca3e71c
marc <info@lifesense-group.com>
2020-04-16 10:25:39 +0200

    t

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.ad89e0643637af6c29a5ad7a7a9795a7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.84b6debaa6bf5503dd24a09436a18f2d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
