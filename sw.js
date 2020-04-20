2ab53f53494cd4b915866e41b9ceb436b9e9e16c
marc <info@lifesense-group.com>
2020-04-20 09:54:20 +0200

    updates

--- sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("precache-manifest.84b6debaa6bf5503dd24a09436a18f2d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-if ('workbox' in self) {
-    workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-  }
