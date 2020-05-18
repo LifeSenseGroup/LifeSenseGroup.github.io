e40fde7ab3fa387ecc049b18f88780dedb3d3980
marc <info@lifesense-group.com>
2020-05-18 15:46:20 +0200

    new colors2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.9f11efbbfe7b12529477dcbe8d708ebc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.242d45406468efa266407855c0da3dbd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
