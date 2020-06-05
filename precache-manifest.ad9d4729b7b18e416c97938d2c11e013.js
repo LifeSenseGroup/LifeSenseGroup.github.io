74dac144b7cb5d6559e2b663026cdb903820d0bb
marc <info@lifesense-group.com>
2020-06-05 08:36:52 +0200

    rel0.2

--- precache-manifest.ad9d4729b7b18e416c97938d2c11e013.js
+++ precache-manifest.ad9d4729b7b18e416c97938d2c11e013.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "dc1c11f2b64a4cc27ea1",
+    "revision": "953ae46535bbb342ba42",
     "url": "main.css"
   },
   {
-    "revision": "dc1c11f2b64a4cc27ea1",
+    "revision": "953ae46535bbb342ba42",
     "url": "bundle.js"
   },
   {
-    "revision": "8e4f2710640c2116451fa26afe9e2313",
+    "revision": "fdd0726b3003cdc5bdc2d9a71359f29d",
     "url": "index.html"
   },
   {
-    "revision": "b7ce95a8fad643ffae1eba9649cf4083",
+    "revision": "5a142a8cbedbb2a5521ce644cc0adf62",
     "url": "styles.css"
   },
   {
@@ -24,20 +24,20 @@ self.__precacheManifest = [
     "url": "manifest.webmanifest"
   },
   {
-    "revision": "e044a63e034bf10304dad73138b8c74b",
-    "url": "vendor/webcomponents-loader.js"
-  },
-  {
     "revision": "894a294495257c3d389efa3e1bd9bde7",
     "url": "vendor/webcomponents-bundle.js"
   },
   {
-    "revision": "fb2608da8f301832531117b05f4acdb3",
-    "url": "vendor/bundles/webcomponents-ce.js"
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "vendor/webcomponents-loader.js"
   },
   {
     "revision": "3a28d79ed7dae933323bcabb6ad3dade",
     "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "fb2608da8f301832531117b05f4acdb3",
+    "url": "vendor/bundles/webcomponents-ce.js"
   },
   {
     "revision": "d0c0741d9f0612fcaf6589ba17a78bd8",
@@ -56,6 +56,10 @@ self.__precacheManifest = [
     "url": "img/icons/favicon.png"
   },
   {
+    "revision": "22cf434c35485a385ee356882fbbacb3",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
     "revision": "bee3e74499f28f1aae40dc0535fbbff8",
     "url": "img/icons/feet2.png"
   },
@@ -64,15 +68,11 @@ self.__precacheManifest = [
     "url": "img/icons/icon-144x144.png"
   },
   {
-    "revision": "22cf434c35485a385ee356882fbbacb3",
-    "url": "img/icons/icon-192x192.png"
+    "revision": "f1959fd93f05d666e61b71aa2c2d3ac2",
+    "url": "img/logo_plan_be.png"
   },
   {
     "revision": "265a4a61a2d0cb79fe2f716e12029a29",
     "url": "img/icons/icon-512x512.png"
-  },
-  {
-    "revision": "f1959fd93f05d666e61b71aa2c2d3ac2",
-    "url": "img/logo_plan_be.png"
   }
 ];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.3404c307b53fa83d0a390ef1671cec20.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.ad9d4729b7b18e416c97938d2c11e013.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
