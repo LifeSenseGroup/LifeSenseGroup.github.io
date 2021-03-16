05949c3e0850a03a946826e4474c60a0d64560e7
marc <info@lifesense-group.com>
2021-03-16 14:00:09 +0100

    removed donwload button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.aac4c226ced61d2c2c327785efd7db57.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.3f74adc97a406369e72f4dcd42db8e04.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
