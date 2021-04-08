2b2d7db79f1d846e4e45f1ae0dde1b9782e88cee
marc <info@lifesense-group.com>
2021-04-08 10:03:27 +0200

    removed icon column of video

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.3f74adc97a406369e72f4dcd42db8e04.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.091f9ee54ad36507964972ec1ee55e29.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
