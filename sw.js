656830480058086b71573df0a981ccd343aa8735
marc <info@lifesense-group.com>
2021-05-04 14:35:01 +0200

    video chat update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.091f9ee54ad36507964972ec1ee55e29.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.3785e4ead211407be65612e40a32cba5.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
