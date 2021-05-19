beb8e9181c6a24337dfb963bbc829166a2af2b26
marc <info@lifesense-group.com>
2021-05-19 14:28:37 +0200

    new version for meeting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.9263d2233a21602cdeb857e5c23cf9a9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.6da337b648af117e7325a4fab8443ad6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
