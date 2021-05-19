44738f452b59b260160c61a9876c726e3c165703
marc <info@lifesense-group.com>
2021-05-19 13:16:41 +0200

    version for investor meeting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3331e461a89aa4cea45004581ad3bda6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.9263d2233a21602cdeb857e5c23cf9a9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
