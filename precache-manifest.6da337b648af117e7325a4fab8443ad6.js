1392edaf817c6e328ff979ddca9fb7a9dcb4fd0b
marc <info@lifesense-group.com>
2021-07-28 11:33:08 +0200

    add precache for service workers

---
+++ precache-manifest.6da337b648af117e7325a4fab8443ad6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb92ad0fd463b4ed2ea5",
+    "url": "/main.css"
+  },
+  {
+    "revision": "bb92ad0fd463b4ed2ea5",
+    "url": "/bundle.56d0658bca81b4df5988.js"
+  },
+  {
+    "revision": "0e6df6cdc740a3c0e3b0348fba3042be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8df20dec6e2eeae914abc32ca606e71",
+    "url": "/styles.css"
+  },
+  {
+    "revision": "c802a321697a93fa2624e671b11b066d",
+    "url": "/json/standard_exercises_codes.json"
+  },
+  {
+    "revision": "79106fd9b808b3bbd70d54b934850301",
+    "url": "/vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "b7379d8a55066cae9b9f3c0c38ab2bad",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "894a294495257c3d389efa3e1bd9bde7",
+    "url": "/vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "/vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "fb2608da8f301832531117b05f4acdb3",
+    "url": "/vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "d0c0741d9f0612fcaf6589ba17a78bd8",
+    "url": "/vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "3a28d79ed7dae933323bcabb6ad3dade",
+    "url": "/vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "e2f0b54cb1564b7467ce56bab4945e77",
+    "url": "/vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "5819a566ea61a172bf8eae5c7c87332f",
+    "url": "/json/en_personalised_exercises.json"
+  },
+  {
+    "revision": "8c0463a3315163a31532aded545c661a",
+    "url": "/json/en_standard_exercises.json"
+  },
+  {
+    "revision": "e42bbcd7565e187cd017fad7df388e01",
+    "url": "/json/personalised_exercises_codes.json"
+  },
+  {
+    "revision": "0421b4b74aa24572c428a97bc19880eb",
+    "url": "/img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "22cf434c35485a385ee356882fbbacb3",
+    "url": "/img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "028be18872917eee9d192f7ef2c7d354",
+    "url": "/img/icons/favicon.png"
+  },
+  {
+    "revision": "2cf7b0cc9199ab8fd07524173ee7ab80",
+    "url": "/img/logo_plan_be.png"
+  },
+  {
+    "revision": "265a4a61a2d0cb79fe2f716e12029a29",
+    "url": "/img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "2c7ed0ad79e860db1a5bdebdacb421e6",
+    "url": "/img/loop-table.docx"
+  }
+];
